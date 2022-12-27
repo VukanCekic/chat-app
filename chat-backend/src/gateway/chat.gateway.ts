9375b12e71d8f7c7c1fb82f42141d0849734095f
VukanCekic <vukanwork@gmail.com>
2022-12-27 16:41:53 +0100

    first commit

---
+++ chat-backend/src/gateway/chat.gateway.ts
@@ -0,0 +1,13 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets/decorators";
+
+@WebSocketGateway(80, { namespace: 'chat' })
+export class ChatGateway{
+   @WebSocketServer()
+   private server;
+   
+   @SubscribeMessage('message')
+   handle(@MessageBody() message: string): void{
+     this.server.emit('message', message);
+   }
+
+}
